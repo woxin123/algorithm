f926bf7d96ca84209b47686fa8c7eb6f8a97a8f8
孟晨 <1293141942@qq.com>
2017-10-07 14:45:34 +0800

    斐波那契和旋转数组寻找最小值

--- src/com/example/剑指offer/AddTwoList.java
+++ src/com/example/剑指offer/AddTwoList.java
@@ -2,6 +2,9 @@ package com.example.剑指offer;
 
 import java.util.Scanner;
 
+/**
+ * 输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+ */
 public class AddTwoList {
     public ListNode Merge(ListNode list1, ListNode list2) {
         if (list1 == null)
