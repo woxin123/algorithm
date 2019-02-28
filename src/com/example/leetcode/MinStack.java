4fc02d0beea4193a7c7db3110929a0410b12c816
woxin <1293141942@qq.com>
2019-02-28 22:00:34 +0800

    最小栈

---
+++ src/com/example/leetcode/MinStack.java
@@ -0,0 +1,50 @@
+package com.example.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author mengchen
+ * @time 19-2-28 下午7:09
+ */
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        if (minStack.isEmpty() || minStack.peek() > x) {
+            minStack.push(x);
+        } else {
+            minStack.push(minStack.peek());
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        minStack.pop();
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
